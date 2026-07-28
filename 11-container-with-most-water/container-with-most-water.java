4005d3864073d922a2b78857b41e4b0ad5505b61
anupkasanya07 <154727396+anupkasanya07@users.noreply.github.com>
2026-07-28 22:06:40 +0530

    Time: 5 ms (82.80%) | Memory: 77.5 MB (29.47%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxWater = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            // Width between the two lines
+            int width = right - left;
+            
+            // Container height is bounded by the shorter line
+            int currentHeight = Math.min(height[left], height[right]);
+            
+            // Calculate current area and update maximum
+            int currentArea = width * currentHeight;
+            maxWater = Math.max(maxWater, currentArea);
+
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+}
