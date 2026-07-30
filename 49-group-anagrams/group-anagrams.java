287d7a9b8030fc8938a26cf47cba44eca07e0295
anupkasanya07 <154727396+anupkasanya07@users.noreply.github.com>
2026-07-30 19:10:07 +0530

    Time: 18 ms (20.70%) | Memory: 50 MB (20.79%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            int[] count = new int[26];
+            for (char c : str.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            // Build unique key from character counts
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append('#').append(count[i]);
+            }
+            String key = sb.toString();
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
