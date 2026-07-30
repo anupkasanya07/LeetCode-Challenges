d216da7573bcc6c73173733f69491ac26837bd19
anupkasanya07 <154727396+anupkasanya07@users.noreply.github.com>
2026-07-30 19:14:52 +0530

    Time: 2 ms (88.87%) | Memory: 48.1 MB (8.72%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            // If current index is beyond the maximum reachable index
+            if (i > maxReach) {
+                return false;
+            }
+
+            // Update the furthest reachable index
+            maxReach = Math.max(maxReach, i + nums[i]);
+
+            // Early exit if we can already reach the end
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+
+        return true;
+    }
+}
