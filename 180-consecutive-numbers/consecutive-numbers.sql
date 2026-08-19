5135ed59069c8a769208352181c575f5501f3eae
anupkasanya07 <154727396+anupkasanya07@users.noreply.github.com>
2026-08-19 09:01:00 +0530

    Time: 617 ms (47.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,16 @@
+WITH CTE AS (
+    SELECT 
+        num,
+        LEAD(num, 1) OVER (ORDER BY id) AS next_num,
+        LEAD(num, 2) OVER (ORDER BY id) AS next_next_num,
+        id,
+        LEAD(id, 1) OVER (ORDER BY id) AS next_id,
+        LEAD(id, 2) OVER (ORDER BY id) AS next_next_id
+    FROM Logs
+)
+SELECT DISTINCT num AS ConsecutiveNums
+FROM CTE
+WHERE num = next_num 
+  AND num = next_next_num
+  AND next_id = id + 1
+  AND next_next_id = id + 2;
